b820883574b2f2569fc143358ce4842a10645d36
pyogoona <pyogoona@gmail.com>
2019-04-18 17:22:16 +0900

    add project files

---
+++ src/test/java/co/kr/sptek/myspringapp/MyspringappApplicationTests.java
@@ -0,0 +1,16 @@
+package co.kr.sptek.myspringapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyspringappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
